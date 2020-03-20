3fdc26301fbb5ba1e262008827eed0872fa53940
Gautham Suresh <gautham.suresh@verloop.io>
2020-03-20 11:39:38 +0530

    Remove stackdriver hacks, cleanup module

--- verloopcontext/verloopcontext/logging.py
+++ verloopcontext/verloopcontext/logging.py
@@ -5,68 +5,56 @@ import sys
 import structlog
 from structlog.stdlib import LoggerFactory, add_log_level
 
-from google.protobuf import message
-from google.protobuf.json_format import MessageToDict
-
-
-
-def stackdriver_hacks(_, __, event_dict):
-    def rename(old, new, default=""):
-        event_dict[new] = event_dict.pop(old, event_dict.get(new, default))
-    rename("event", "message", "")
-    rename("level", "severity", "info")
-
-    def convert_protopuf_to_dict(obj):
-        new_obj = obj
-        if isinstance(obj, dict):
-            new_obj = {}
-            for key in obj:
-                new_obj[key] = convert_protopuf_to_dict(obj[key])
-        elif isinstance(obj, list):
-            new_obj = []
-            new_obj = [convert_protopuf_to_dict(item) for item in obj]
-        elif isinstance(obj, message.Message):
-            new_obj = MessageToDict(obj)
-        return new_obj
-
-    event_dict = convert_protopuf_to_dict(event_dict)
-
-    return event_dict
-
-
 _configured = False
 
 
-def configure(force=False):
+def configure(force = False):
+    """
+    Configures logging & structlog modules
+    """
+
     global _configured
     if _configured and not force:
         return
-    debug = os.environ.get("VADER_DEBUG", False)
+
+    # Check whether debug flag is set
+    debug = os.environ.get('DEBUG_MODE', False)
+    # Set appropriate log level
     if debug:
-        lvl = logging.DEBUG
+        log_level = logging.DEBUG
     else:
-        lvl = logging.INFO
+        log_level = logging.INFO
+
+    # Set logging config
     logging.basicConfig(
-        level=lvl,
-        #stream=sys.stdout,
-        format="%(message)s",
+        level = log_level,
+        format = "%(message)s",
     )
 
+    # Configure structlog
     structlog.configure(
-        logger_factory=LoggerFactory(),
-        processors=[
+        logger_factory = LoggerFactory(),
+        processors = [
             add_log_level,
-            structlog.processors.TimeStamper("iso"),
-            stackdriver_hacks,
+            # Add timestamp
+            structlog.processors.TimeStamper('iso'),
+            # Add stack information
             structlog.processors.StackInfoRenderer(),
+            # Set exception field using exec info
             structlog.processors.format_exc_info,
+            # Render event_dict as JSON
             structlog.processors.JSONRenderer()
         ]
     )
 
+    _configured = True
+
 
 def get_logger(**kwargs):
+    """
+    Get the structlog logger
+    """
+    # Configure logging modules
     configure()
-    if 'version' not in kwargs:
-        kwargs['version'] =  os.environ.get("VERSION", "unknown")
+    # Return structlog
     return structlog.get_logger(**kwargs)
