4074f47da3da90e347a700df465cd806e21b25ae
Gautham Suresh <gautham.suresh@verloop.io>
2020-05-16 20:16:36 +0530

    Fix twirpy examples

--- example/server.py
+++ example/server.py
@@ -3,14 +3,14 @@ import random
 from twirp.asgi import TwirpASGIApp
 from twirp.exceptions import InvalidArgument
 
-from . import haberdasher_twirp, haberdasher_pb2
+from generated import haberdasher_twirp, haberdasher_pb2
 
 class HaberdasherService(object):
     def MakeHat(self, context, size):
         if size.inches <= 0:
             raise InvalidArgument(argument="inches", error="I can't make a hat that small!")
         return haberdasher_pb2.Hat(
-            inches=size.inches,
+            size=size.inches,
             color= random.choice(["white", "black", "brown", "red", "blue"]),
             name=random.choice(["bowler", "baseball cap", "top hat", "derby"])
         )
