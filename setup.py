a26b6fa5fa540b41f9d261547ac61538e7724b46
avinassh <avinassh@users.noreply.github.com>
2020-12-13 19:34:26 +0530

    Update package settings to display proper README on PyPi (#28)

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup
 with open('version.txt') as f:
     version = f.read().strip()
 
+with open('README.md', encoding='utf-8') as f:
+      long_description = f.read().strip()
+
 setup(name="twirp",
       version=version,
       description="Twirp server and client lib",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       licesnse='unlicense',
       packages=['twirp'],
       install_requires=[
