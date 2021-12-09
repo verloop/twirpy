53ca044a70fb69f504f4f056700f1e528d69906e
chadawagner <chadawagner@users.noreply.github.com>
2021-12-08 22:12:44 -0800

    Handle JSON decode error when parsing error response in client (#35)
    * handle JSON decode error when parsing error response
    
    * catch only JSONDecodeError
    
    * raise exception from None
    
    * handle errors from intermediate proxies in client
    
    Co-authored-by: Chad Wagner <chad@kadenze.com>

--- twirp/client.py
+++ twirp/client.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from . import exceptions
@@ -22,8 +23,10 @@ class TwirpClient(object):
                 response = response_obj()
                 response.ParseFromString(resp.content)
                 return response
-            else:
+            try:
                 raise exceptions.TwirpServerException.from_json(resp.json())
+            except json.JSONDecodeError:
+                raise self._twirp_error_from_intermediary(resp) from None
             # Todo: handle error
         except requests.exceptions.Timeout as e:
             raise exceptions.TwirpServerException(
@@ -37,3 +40,42 @@ class TwirpClient(object):
                 message=str(e),
                 meta={"original_exception": e},
             )
+
+    @staticmethod
+    def _twirp_error_from_intermediary(resp):
+        # see https://twitchtv.github.io/twirp/docs/errors.html#http-errors-from-intermediary-proxies
+        meta = {
+            'http_error_from_intermediary': 'true',
+            'status_code': str(resp.status_code),
+        }
+
+        if resp.is_redirect:
+            # twirp uses POST which should not redirect
+            code = errors.Errors.Internal
+            location = resp.headers.get('location')
+            message = 'unexpected HTTP status code %d "%s" received, Location="%s"' % (
+                resp.status_code,
+                resp.reason,
+                location,
+            )
+            meta['location'] = location
+
+        else:
+            code = {
+                400: errors.Errors.Internal,  # JSON response should have been returned
+                401: errors.Errors.Unauthenticated,
+                403: errors.Errors.PermissionDenied,
+                404: errors.Errors.BadRoute,
+                429: errors.Errors.ResourceExhausted,
+                502: errors.Errors.Unavailable,
+                503: errors.Errors.Unavailable,
+                504: errors.Errors.Unavailable,
+            }.get(resp.status_code, errors.Errors.Unknown)
+
+            message = 'Error from intermediary with HTTP status code %d "%s"' % (
+                resp.status_code,
+                resp.reason,
+            )
+            meta['body'] = resp.text
+
+        return exceptions.TwirpServerException(code=code, message=message, meta=meta)
