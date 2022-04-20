afb3d4a07c5bea3df0014d04d7dab1292880db62
Piyush <piyush@codeitout.com>
2022-04-20 09:40:04 +0530

    Add an example for optional field

--- example/server.py
+++ example/server.py
@@ -9,11 +9,14 @@ class HaberdasherService(object):
     def MakeHat(self, context, size):
         if size.inches <= 0:
             raise InvalidArgument(argument="inches", error="I can't make a hat that small!")
-        return haberdasher_pb2.Hat(
+        response = haberdasher_pb2.Hat(
             size=size.inches,
-            color= random.choice(["white", "black", "brown", "red", "blue"]),
-            name=random.choice(["bowler", "baseball cap", "top hat", "derby"])
+            color=random.choice(["white", "black", "brown", "red", "blue"]),
         )
+        if random.random() > 0.5:
+            response.name = random.choice(["bowler", "baseball cap", "top hat", "derby"])
+
+        return response
 
 
 service = haberdasher_twirp.HaberdasherServer(
