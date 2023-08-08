53c38c95600405620143b0b40a5e9799cf5f2f45
chadawagner <chadawagner@users.noreply.github.com>
2023-08-07 23:21:46 -0700

    create/close single aiohttp session

--- twirp/async_client.py
+++ twirp/async_client.py
@@ -1,41 +1,47 @@
+import asyncio
 import json
 import aiohttp
 
 from . import exceptions
 from . import errors
 
-
 class AsyncTwirpClient:
     def __init__(self, address, timeout=5):
         self._address = address
         self._timeout = timeout
-        self._http_session = None
+        self._session = None
+
+    def __del__(self):
+        if self._session:
+            asyncio.create_task(self._session.close())
 
     @property
-    def http_session(self) -> aiohttp.ClientSession:
-        if self._http_session is None:
-            self._http_session = aiohttp.ClientSession(
+    def session(self):
+        if self._session is None:
+            self._session = aiohttp.ClientSession(
                 self._address, timeout=aiohttp.ClientTimeout(total=self._timeout))
-        return self._http_session
+        return self._session
 
-    async def _make_request(self, *args, url, ctx, request, response_obj, **kwargs):
+    async def _make_request(self, *, url, ctx, request, response_obj, session=None, **kwargs):
         headers = ctx.get_headers()
         if 'headers' in kwargs:
             headers.update(kwargs['headers'])
         kwargs['headers'] = headers
         kwargs['headers']['Content-Type'] = 'application/protobuf'
         try:
-            resp = await self.http_session.post(url=url, data=request.SerializeToString(), **kwargs)
-            if resp.status == 200:
-                response = response_obj()
-                response.ParseFromString(resp.content)
-                return response
-            try:
-                raise exceptions.TwirpServerException.from_json(await resp.json())
-            except (aiohttp.ContentTypeError, json.JSONDecodeError):
-                raise exceptions.twirp_error_from_intermediary(
-                    resp.status, resp.reason, resp.headers, await resp.text) from None
-            # Todo: handle error
+            async with await (session or self.session).post(
+                url=url, data=request.SerializeToString(), **kwargs
+            ) as resp:
+                if resp.status == 200:
+                    response = response_obj()
+                    response.ParseFromString(await resp.read())
+                    return response
+                try:
+                    raise exceptions.TwirpServerException.from_json(await resp.json())
+                except (aiohttp.ContentTypeError, json.JSONDecodeError):
+                    raise exceptions.twirp_error_from_intermediary(
+                        resp.status, resp.reason, resp.headers, await resp.text()
+                    ) from None
         except aiohttp.ServerTimeoutError as e:
             raise exceptions.TwirpServerException(
                 code=errors.Errors.DeadlineExceeded,
