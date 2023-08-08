814dff4db913330a54f35afc0c88916c7ca0306c
chadawagner <chadawagner@users.noreply.github.com>
2023-08-08 12:08:17 -0700

    session param in init, better teardown and examples

--- example/client.py
+++ example/client.py
@@ -1,5 +1,6 @@
 try:
     import asyncio
+    import aiohttp
 except ModuleNotFoundError:
     pass
 
@@ -9,8 +10,12 @@ from twirp.exceptions import TwirpServerException
 from generated import haberdasher_twirp, haberdasher_pb2
 
 
+server_url = "http://localhost:3000"
+timeout_s = 5
+
+
 def main():
-    client = haberdasher_twirp.HaberdasherClient("http://localhost:3000")
+    client = haberdasher_twirp.HaberdasherClient(server_url, timeout_s)
 
     try:
         response = client.MakeHat(
@@ -26,7 +31,7 @@ def main():
 
 
 async def async_main():
-    client = haberdasher_twirp.AsyncHaberdasherClient("http://localhost:3000")
+    client = haberdasher_twirp.AsyncHaberdasherClient(server_url, timeout_s)
 
     try:
         response = await client.MakeHat(
@@ -41,10 +46,41 @@ async def async_main():
         print(e.code, e.message, e.meta, e.to_dict())
 
 
+async def async_with_session():
+    # It is optional but recommended to provide your own ClientSession to the twirp client
+    # either on init or per request, and ensure it is closed properly on app shutdown.
+    # Otherwise, the client will create its own session to use, which it will attempt to
+    # close in its __del__ method, but has no control over how or when that will get called.
+
+    # NOTE: ClientSession may only be created (or closed) within a coroutine.
+    session = aiohttp.ClientSession(
+        server_url, timeout=aiohttp.ClientTimeout(total=timeout_s)
+    )
+
+    # If session is provided, session controls the timeout. Timeout parameter to client init is unused
+    client = haberdasher_twirp.AsyncHaberdasherClient(server_url, session=session)
+
+    try:
+        response = await client.MakeHat(
+            ctx=Context(),
+            request=haberdasher_pb2.Size(inches=12),
+            server_path_prefix="/twirpy",
+            # Optionally provide a session per request
+            # session=session,
+        )
+        if not response.HasField("name"):
+            print("We didn't get a name!")
+        print(response)
+    except TwirpServerException as e:
+        print(e.code, e.message, e.meta, e.to_dict())
+    finally:
+        # Close the session (could also use a context manager)
+        await session.close()
+
+
 if __name__ == "__main__":
     if hasattr(haberdasher_twirp, "AsyncHaberdasherClient"):
         print("using async client")
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(async_main())
+        asyncio.run(async_main())
     else:
         main()
--- twirp/async_client.py
+++ twirp/async_client.py
@@ -6,20 +6,29 @@ from . import exceptions
 from . import errors
 
 class AsyncTwirpClient:
-    def __init__(self, address, timeout=5):
+    def __init__(self, address, timeout=5, session=None):
         self._address = address
         self._timeout = timeout
-        self._session = None
+        self._session = session
+        self._should_close_session = False
 
     def __del__(self):
-        if self._session:
-            asyncio.create_task(self._session.close())
+        if self._should_close_session:
+            try:
+                loop = asyncio.get_event_loop()
+                if loop.is_running():
+                    loop.create_task(self._session.close())
+                elif not loop.is_closed():
+                    loop.run_until_complete(self._session.close())
+            except RuntimeError:
+                pass
 
     @property
     def session(self):
         if self._session is None:
             self._session = aiohttp.ClientSession(
                 self._address, timeout=aiohttp.ClientTimeout(total=self._timeout))
+            self._should_close_session = True
         return self._session
 
     async def _make_request(self, *, url, ctx, request, response_obj, session=None, **kwargs):
@@ -42,10 +51,10 @@ class AsyncTwirpClient:
                     raise exceptions.twirp_error_from_intermediary(
                         resp.status, resp.reason, resp.headers, await resp.text()
                     ) from None
-        except aiohttp.ServerTimeoutError as e:
+        except asyncio.TimeoutError as e:
             raise exceptions.TwirpServerException(
                 code=errors.Errors.DeadlineExceeded,
-                message=str(e),
+                message=str(e) or "request timeout",
                 meta={"original_exception": e},
             )
         except aiohttp.ServerConnectionError as e:
