46513440a32fa6cdf5b4789c3843765ac392a9a0
Edward Paget <edward.paget@chime.com>
2024-01-29 10:17:01 -0600

    protoc-gen-twirpy: add a test for main function

--- protoc-gen-twirpy/go.mod
+++ protoc-gen-twirpy/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.5.0
+	github.com/stretchr/testify v1.8.4 // indirect
 	google.golang.org/protobuf v1.28.0
 )
---
+++ protoc-gen-twirpy/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func loadProto(t *testing.T) *os.File {
+	file, err := os.Open("test_data/protoc_input.data")
+	require.NoError(t, err)
+	return file
+}
+
+func loadGenerated(t *testing.T) []byte {
+	file, err := os.Open("../example/generated/haberdasher_twirp.py")
+	require.NoError(t, err)
+	result, err := ioutil.ReadAll(file)
+	require.NoError(t, err)
+	return result
+}
+
+func captureOutput(t *testing.T, f func()) []byte {
+	orig := os.Stdout
+	r, w, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = w
+	f()
+	os.Stdout = orig
+	w.Close()
+	out, err := ioutil.ReadAll(r)
+	require.NoError(t, err)
+	return out
+}
+
+func TestMain(t *testing.T) {
+	os.Stdin = loadProto(t)
+	result := captureOutput(t, main)
+	assert.Equal(t, "\x10\x01z\xc8\r\n\x14haberdasher_twirp.pyz\xaf\r", string(result[:30]))
+	assert.Equal(t, loadGenerated(t), result[30:]) // skip the first 30 bytes which are the header
+}
